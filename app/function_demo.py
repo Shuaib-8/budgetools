917cac5c8c8d944bcd0163a303778714d078dd26
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2022-10-14 13:00:53 +0100

    update code for mvp streamlit app

--- app/function_demo.py
+++ app/function_demo.py
@@ -1,27 +1,25 @@
-import streamlit as st 
-import pandas as pd 
-import numpy_financial as npf
-from pathlib import Path
-import os
+import streamlit as st
 from main import monthly_amount_to_investment
 
-# Title to app 
-st.title('Welcome to a calculator for monthly savings given financial target')
+# Title to app
+st.title("Welcome to a calculator for monthly savings given financial target")
 
-rate = st.number_input('Enter assumed average rate of return (RoR)', min_value=0.00, max_value=1.00)
+rate = st.number_input(
+    "Enter assumed average rate of return (RoR)", min_value=0.00,
+    max_value=1.00
+)
 
-years = st.number_input('Enter the number of years you would like to invest for', min_value=1, max_value=50)
+years = st.number_input(
+    "Enter the number of years you would like to invest for", min_value=1,
+    max_value=50
+)
 
-target_amount = st.number_input('Enter the target amount that you would like to aim for', min_value=1000, step=1000)
+target_amount = st.number_input(
+    "Enter the target amount that you would like to aim for", min_value=1000,
+    step=1000
+)
 
-if (st.button('Calculate the amount to invest per month')):
+if st.button("Calculate the amount to invest per month"):
 
     amount = monthly_amount_to_investment(rate, years, target_amount)
-    st.markdown(f'**The amount to invest (per month) is:** \n\n{amount:.2f}')
-
-
-
-
-
-
-
+    st.markdown(f"**The amount to invest (per month) is:** \n\n{amount:.2f}")
