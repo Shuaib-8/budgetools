5421cce5ffe08261c251ccf90c6f69f3b3c05d6e
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2022-12-17 12:08:56 +0000

    Update SalaryExpensesForecasting class with docstrings

--- budgetools/budgetools/forecast.py
+++ budgetools/budgetools/forecast.py
@@ -4,7 +4,41 @@ from budgetools.budget import BaseBudget
 
 
 class SalaryExpensesForecasting(BaseBudget):
+    """
+    Forecasting instance class for estimate future flows for
+    1. Salary - Salary growth over given period.
+    2. Expenses - Expenses (cost of living) over given period.
+
+    Attributes
+    ----------
+    `years` : int
+        The number of years (period) to estimate forecasts for.
+    `salary` : int
+        Annual salary (gross).
+    `tax_rate` : float
+        Tax rate percentage as a decimal e.g. 30% = 0.3.
+
+    Methods
+    -------
+    `monthly_salary_forecast`
+        Computes an estimate of prospective monthly salary flows.
+    `monthly_expenses_forecast`
+        Computes an estimate of prospective monthly expenses (cost of living) flows.
+    """
+
     def __init__(self, years, salary, tax_rate):
+        """
+        Instantiates the attributes for the SalaryExpensesForecasting object.
+
+        Parameters
+        ----------
+        `years` : int
+            Sets the number of years of the SalaryExpensesForecasting object.
+        `salary` : int
+            Sets salary of the SalaryExpensesForecasting object.
+        `tax_rate` : float
+            Sets tax rate of the SalaryExpensesForecasting object.
+        """
         super().__init__(salary, tax_rate)
         self.years = years
         self._annual_salary_growth = 0
@@ -12,6 +46,16 @@ class SalaryExpensesForecasting(BaseBudget):
 
     @property
     def annual_salary_growth(self) -> float:
+        """
+        The 'annual_salary_growth' property. Get or set the annual salary growth
+        property value.
+        The suitable value lies between 0-1.
+
+        Returns
+        -------
+        float
+            The annual salary growth rate.
+        """
         return self._annual_salary_growth
 
     @annual_salary_growth.setter
@@ -22,6 +66,16 @@ class SalaryExpensesForecasting(BaseBudget):
 
     @property
     def annual_inflation(self) -> float:
+        """
+        The 'annual_inflation' property. Get or set the annual inflation rate
+        property value.
+        The suitable value lies between 0-1.
+
+        Returns
+        -------
+        float
+            The annual (CPI) inflation rate.
+        """
         return self._annual_inflation
 
     @annual_inflation.setter
@@ -30,7 +84,29 @@ class SalaryExpensesForecasting(BaseBudget):
             raise ValueError("The inflation rate should be 0 or between 0-1")
         self._annual_inflation = inflation_value
 
-    def monthly_salary_forecast(self):
+    def monthly_salary_forecast(self) -> np.ndarray:
+        """
+        Projects the flows for the monthly salary forecast over the defined period and
+        estimated (average) annual salary growth.
+
+        Returns
+        -------
+        numpy.ndarray
+            Returns the flow of monthly salary forecast, where each element in the array
+            is an estimate of the forecasted salary value, starting from month 1 until
+            end of array which is equal to length of years value converted into months
+            e.g. 15 years is equal to 180 months = 180 elements in the array.
+
+        Examples
+        --------
+        >>> from budgetools.forecast import SalaryExpensesForecasting
+        >>> forecast_living = SalaryExpensesForecasting(years=15, salary=60000,
+                                                        tax_rate=0.3)
+        >>> forecast_living.annual_salary_growth = 0.05
+        >>> forecast_living.monthly_salary_forecast()
+        array([3514.26, 3528.58, 3542.95, 3557.39, 3571.88, 3586.43, 3601.04, \
+        3615.72, 3630.45, 3645.24, 3660.09, 3675, ...])
+        """
         MONTHS_PER_YEAR = 12
         # Take the years and convert to months as a forecast parameter
         forecast_months = MONTHS_PER_YEAR * self.years
@@ -52,7 +128,34 @@ class SalaryExpensesForecasting(BaseBudget):
 
         return salary_forecast
 
-    def monthly_expenses_forecast(self):
+    def monthly_expenses_forecast(self) -> np.ndarray:
+        """
+        Projects the flows for the monthly expenses (cost of living) forecast over the
+        defined period and given expense categories.
+
+        Returns
+        -------
+        numpy.ndarray
+            Returns the flow of monthly expenses forecast, where each element in the
+            array is an estimate of the forecasted salary value, starting from month 1
+            until end of array which is equal to length of years value converted into
+            months e.g. 15 years is equal to 180 months = 180 elements in the array.
+
+        Examples
+        --------
+        >>> from budgetools.forecast import SalaryExpensesForecasting
+        >>> forecast_living = SalaryExpensesForecasting(years=15, salary=60000,
+                                                        tax_rate=0.3)
+        >>> forecast_living.annual_inflation = 0.025
+        >>> forecast_living.food_daily = 10
+        >>> forecast_living.entertainment = 200
+        >>> forecast_living.rent = 1200
+        >>> forecast_living.entertainment = 200
+        >>> forecast_living.emergency_expenses = 250
+        >>> forecast_living.monthly_expenses_forecast()
+        array([1954.02, 1958.04, 1962.07, 1966.12, 1970.17, 1974.22, 1978.29, \
+        1982.37, 1986.45, 1990.54, 1994.64, 1998.75, ...])
+        """
         MONTHS_PER_YEAR = 12
         # Take the years and convert to months as a forecast parameter
         forecast_months = MONTHS_PER_YEAR * self.years
